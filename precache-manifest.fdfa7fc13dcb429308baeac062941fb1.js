dbb19e5b90ca532938de088ff85026e03601d76d
Sheikh-Tafsir <190041130tafsir@gmail.com>
2023-05-25 01:17:32 +0600

    Updates

---
+++ precache-manifest.fdfa7fc13dcb429308baeac062941fb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "780c640f45a2a23e9b96",
+    "url": "/STRfolio/static/css/main.6eaa8058.chunk.css"
+  },
+  {
+    "revision": "780c640f45a2a23e9b96",
+    "url": "/STRfolio/static/js/main.780c640f.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/STRfolio/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "4a546aff8428c77d2509",
+    "url": "/STRfolio/static/css/2.4111dbe4.chunk.css"
+  },
+  {
+    "revision": "4a546aff8428c77d2509",
+    "url": "/STRfolio/static/js/2.4a546aff.chunk.js"
+  },
+  {
+    "revision": "7efc4d1dfdcfd62f478d",
+    "url": "/STRfolio/static/js/runtime~main.7efc4d1d.js"
+  },
+  {
+    "revision": "4b3b37466125ff0db49c0151e1164287",
+    "url": "/STRfolio/index.html"
+  }
+];
